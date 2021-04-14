d6addd31b853d2defc388e503675b58ec1e02072
Darshan Patel <darshan.patel@kiprosh.com>
2021-04-14 07:32:05 +0530

    SignIn using Google (#16)
    * Google Sign in implemented
    
    * func for setting parameter
    
    * typo fix
    
    * Code optimization
    
    * Fix PR suggestion
    
    * Fix conflicts
    
    * Remove unwanted files
    
    * Move client id
    
    * Secure google scheme url

---
+++ Reward/Reward/Controllers/SignIn/SignInViewController.swift
@@ -0,0 +1,64 @@
+//
+//  SignInViewController.swift
+//  Reward
+//
+//  Created by Darshan on 08/04/21.
+//
+
+import UIKit
+import GoogleSignIn
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    @IBOutlet weak var signOutButton: UIButton!
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setGoogleSignIn()
+        updateScreenOnLoginStatus()
+    }
+    
+    func setGoogleSignIn() {
+        GIDSignIn.sharedInstance().delegate = self
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        NotificationCenter.default.addObserver(self, selector: #selector(userDidSignInGoogle(_:)), name: .signInGoogleCompleted, object: nil)
+        signInButton.style = .wide
+    }
+
+    
+    @objc private func userDidSignInGoogle(_ notification: Notification) {
+        updateScreenOnLoginStatus()
+    }
+    
+    private func updateScreenOnLoginStatus() {
+        if let user = GIDSignIn.sharedInstance()?.currentUser {
+            userNameLabel.text = user.profile.givenName
+            signInButton.isEnabled = false
+            signOutButton.isEnabled = true
+        } else {
+            userNameLabel.text = String()
+            signInButton.isEnabled = true
+            signOutButton.isEnabled = false
+        }
+    }
+    
+    func getParameters() -> [String: Any] {
+        var parameter = [String: Any]()
+        guard let user = GIDSignIn.sharedInstance()?.currentUser else {
+            return parameter
+        }
+        parameter[APIKeys.SignIn.firstName] = user.profile.givenName
+        parameter[APIKeys.SignIn.lastName] = user.profile.familyName
+        parameter[APIKeys.SignIn.email] = user.profile.email
+        parameter[APIKeys.SignIn.googleUid] = user.userID
+        return [APIKeys.SignIn.userAuth: parameter]
+    }
+    
+    @IBAction func signOutButtonAction(_ sender: UIButton) {
+        GIDSignIn.sharedInstance()?.signOut()
+        updateScreenOnLoginStatus()
+    }
+
+}
