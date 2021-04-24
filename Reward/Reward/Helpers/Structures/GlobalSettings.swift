db564bce8dded1d0a5826d06f9b41c106f3a03b5
kevalvadoliya <keval.vadoliya@kiprosh.com>
2021-04-24 17:03:36 +0530

    implement tab bar controller

--- Reward/Reward/Helpers/Structures/GlobalSettings.swift
+++ Reward/Reward/Helpers/Structures/GlobalSettings.swift
@@ -5,9 +5,12 @@
 //  Created by Keval Vadoliya on 08/04/21.
 //
 
-import Foundation
+import UIKit
 
 class GlobalSettings {
+    
     static let timeoutIntervalForRequest: TimeInterval = 60
     static let googleClientId = "839765622136-mbout8i9rt7gg15n4mcp9p9d8isc2svp.apps.googleusercontent.com"
+    static let tabBarShadowSize: CGSize = CGSize(width: 40, height: 40)
+    
 }
