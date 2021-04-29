e849c7b10680e2c5d89391056b7dde959aa02bf4
Darshan Patel <darshan.patel@kiprosh.com>
2021-04-29 21:22:14 +0530

    remove unwanted space

--- Reward/Reward/Controllers/AppDelegate.swift
+++ Reward/Reward/Controllers/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         GIDSignIn.sharedInstance().clientID = GlobalSettings.googleClientId
         GIDSignIn.sharedInstance()?.restorePreviousSignIn()
-        AppSettings().configureTabBar() 
+        AppSettings().configureTabBar()
         return true
     }
 
